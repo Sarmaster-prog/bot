277747dc27ccd558d3fcb210e9e2f251f36959ea
User <srozkov63@gmail.com>
2023-09-08 20:51:18 +0300

    регистрируем хендлеры

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
-API_TOKEN: str = '6406018096:AAFOPLIfEXVlMDHr3p-1eZW9ROsUFcfqI-8'
+API_TOKEN: str = 'BOT TOKEN HERE'
 
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
-@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
 
-@dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
+
+# Регистрируем хэндлеры
+dp.message.register(process_start_command, Command(commands=["start"]))
+dp.message.register(process_help_command, Command(commands=['help']))
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
